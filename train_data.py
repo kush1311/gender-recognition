284d62dc235c33a34abe682943659b9424d51061
kush1311 <116818040+kush1311@users.noreply.github.com>
2024-01-25 21:44:38 +0530

    Update train_data.py

--- train_data.py
+++ train_data.py
@@ -5,7 +5,9 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 train_man_dir = "/content/faces/man"
 train_woman_dir = "/content/faces/woman"
 
-datagen = ImageDataGenerator(rescale=1./255)
+datagen = ImageDataGenerator(rescale=1./255,
+                             horizontal_flip=True,
+                             vertical_flip=True)
 
 train_generator = datagen.flow_from_directory(
     "/content/faces",
@@ -20,11 +22,12 @@ for layer in base_model.layers:
     layer.trainable = False
 
 flat = layers.Flatten()(base_model.output)
+
 output = layers.Dense(1, activation='sigmoid')(flat)
 model = models.Model(inputs=base_model.inputs, outputs=output)
 
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-model.fit(train_generator, epochs=10)
+model.fit(train_generator, epochs=)
 
 model.save("face_data.h5")
